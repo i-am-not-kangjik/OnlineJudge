9c305da3c7346cace84656a344d5a4a149696f57
kangjik <rkqqkb@gmail.com>
2021-11-08 22:01:30 +0900

    백준 단계별로 풀어보기 step5
    백준 단계별로 풀어보기 step5를 풀어보았다.

---
+++ Baekjoon/StepByStep/step5/8958.py
@@ -0,0 +1,20 @@
+a = int(input())
+test = []
+score = []
+for i in range(0, a):
+    s = input()
+    test.append(s)
+
+for i in range(0, a):
+    sum = 0
+    count = 1
+    for j in range(0, len(test[i])):
+        if test[i][j] == 'O':
+            sum += count
+            count += 1
+        else:
+            count = 1
+    score.append(sum)
+
+for i in range(0, len(score)): 
+    print(score[i])
