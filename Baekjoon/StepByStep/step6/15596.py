e4b00693f2635dd0840fc1baca13065b854e7a01
kangjik <rkqqkb@gmail.com>
2021-11-10 21:35:47 +0900

    백준 단계별로 풀어보기 step6
    백준 단계별로 풀어보기 step6 풀이

---
+++ Baekjoon/StepByStep/step6/15596.py
@@ -0,0 +1,6 @@
+def solve(a):
+    sum = 0
+    for i in range(0, len(a)):
+        sum += a[i]
+    return sum
+    
---
+++ Baekjoon/StepByStep/step6/4673.py
@@ -0,0 +1,5 @@
+# 미완성
+def solve():
+    d = 1
+    while(d < 10000):
+        if d < 10:
