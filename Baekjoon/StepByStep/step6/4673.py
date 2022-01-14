18dc5855a71d60f484fb89730f07c512b9897d96
kangjik <rkqqkb@gmail.com>
2022-01-14 22:17:45 +0900

    백준 단계별로 풀어보기 Step6
    백준 단계별로 풀어보기 step6 풀이

--- Baekjoon/StepByStep/step6/4673.py
+++ Baekjoon/StepByStep/step6/4673.py
@@ -1,6 +1,33 @@
-# 미완성
-def solve():
-    d = 1
-    while(d < 10000):
-        if d < 10:
-            d + 1
+list = []
+for i in range(0, 10001):
+    n = i
+    if n < 10:
+        j = n
+        j = j + n
+        if j not in list:
+            list.append(j)
+    elif n > 9 and n < 100:
+        j = n
+        j = j + (n % 10)
+        j = j + (n // 10)
+        if j not in list:
+            list.append(j)
+    elif n > 99 and n < 1000:
+        j = n
+        j = j + (n // 100)
+        j = j + ((n % 100) // 10)
+        j = j + (n % 10)
+        if j not in list:
+            list.append(j)
+    elif n > 999 and n < 10000:
+        j = n
+        j = j + (n // 1000)
+        j = j + ((n % 1000) // 100)
+        j = j + ((n % 100) // 10)
+        j = j + (n % 10)
+        if j not in list:
+            list.append(j)
+            
+for i in range(0, 10001):
+    if i not in list:
+        print(i)
