a0dd83df735dc8f4ecb82024bad990b466309fa1
kangjik <rkqqkb@gmail.com>
2022-01-29 18:46:25 +0900

    백준 단계별로 풀어보기 step8
    백준 단계별로 풀어보기 step8 풀이

---
+++ Baekjoon/StepByStep/step8/2869.py
@@ -0,0 +1,14 @@
+# 백준 2869번 문제
+# 달팽이는 올라가고 싶다
+# https://www.acmicpc.net/problem/2869
+import sys
+import math
+
+def solve():
+    A, B, V = map(int, sys.stdin.readline().split())
+    days = (V - B) / ( A - B)
+    if type(days) == int:
+        print(days)
+    else:
+        print(math.ceil(days))
+solve()
