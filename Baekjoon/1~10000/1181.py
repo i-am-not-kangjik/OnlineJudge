f99aee073d6b8fc2746d18cdc9bfcc15fd48c57a
kangjik <rkqqkb@gmail.com>
2022-11-04 12:22:31 +0900

    백준 단계별로 풀어보기 1181번 문제 람다식 풀이 추가

--- Baekjoon/1~10000/1181.py
+++ Baekjoon/1~10000/1181.py
@@ -14,8 +14,10 @@ for _ in range(t):
     
 li = list(li)
 
-li.sort() # 사전 순 정렬
-li.sort(key=len) # 길이가 짧은 것 부터
+# li.sort() # 사전 순 정렬
+# li.sort(key=len) # 길이가 짧은 것 부터
+
+li.sort(key = lambda x : (len(x), x)) # 길이가 짧은 순, 길이가 같으면 사전순 정렬하는 Lambda식 표현
 
 
 for item in li:
