5d01507010a2358db36115452a7af7ca9594923f
Kangjik Kim <rkqqkb@gmail.com>
2023-03-30 10:12:00 +0900

    프로그래머스 12906번 풀이

---
+++ Programmers/12906/12906.java
@@ -0,0 +1,20 @@
+import java.lang.ProcessBuilder.Redirect.Type;
+import java.util.*;
+
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> tmp = new ArrayList<>();
+        tmp.add(arr[0]);
+        int tmpCnt = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != tmp.get(tmpCnt)){
+                tmp.add(arr[i]);
+                tmpCnt += 1;
+            }
+        }
+        //ArrayList를 int 배열로 변한
+        int[] answer = tmp.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+}
