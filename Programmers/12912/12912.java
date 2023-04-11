8df174e62025267f6e0ff113a8e9ee96b0a3206a
Kangjik Kim <rkqqkb@gmail.com>
2023-04-11 11:49:14 +0900

    프로그래머스 12912번 풀이 복기

--- Programmers/12912/12912.java
+++ Programmers/12912/12912.java
@@ -1,16 +1,35 @@
+// 내 풀이
+// class Solution {
+//     public long solution(int a, int b) {
+//         long answer = 0;
+//         if (a == b){
+//             return a;
+//         }
+//         else if (b < a){ // a가 더 큰 경우 스왑
+//             int tmp;
+//             tmp = a;
+//             a = b;
+//             b = tmp;
+//         }
+//         for (int i = a; i < b+1; i++){
+//             answer += i;
+//         }
+//         return answer;
+//     }
+// }
+
+// 다른 사람의 풀이를 참고한 풀이
 class Solution {
     public long solution(int a, int b) {
-        long answer = 0;
         if (a == b){
             return a;
         }
-        else if (b < a){ // a가 더 큰 경우 스왑
-            int tmp;
-            tmp = a;
-            a = b;
-            b = tmp;
-        }
-        for (int i = a; i < b+1; i++){
+        
+        long answer = 0;
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+
+        for (int i = start; i < end+1; i++){
             answer += i;
         }
         return answer;
