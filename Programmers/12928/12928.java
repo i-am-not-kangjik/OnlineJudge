7f2143c4f5141833fbf441d9b365abfd9fadcb97
Kangjik Kim <rkqqkb@gmail.com>
2023-04-11 12:39:20 +0900

    프로그래머스 12928번 복기

--- Programmers/12928/12928.java
+++ Programmers/12928/12928.java
@@ -1,11 +1,26 @@
+// // 내가 한 풀이 0.05, 75MB
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         for (int i = 1; i < n + 1; i++) {
+//             if (n % i == 0){
+//                 answer += i;
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
+// 다른 사람의 풀이 참고 0.05, 75MB (비슷함)
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = 1; i < n + 1; i++) {
+        for (int i = 1; i < n / 2 + 1; i++) {
             if (n % i == 0){
                 answer += i;
             }
         }
+        answer += n;
         return answer;
     }
 }
