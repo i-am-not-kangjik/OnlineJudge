1cea3e1b3d723050067574785bf137f7b4ae8995
Kangjik Kim <rkqqkb@gmail.com>
2023-04-12 08:59:36 +0900

    프로그래머스 12906번 주석 추가

--- Programmers/12906/12906.java
+++ Programmers/12906/12906.java
@@ -1,4 +1,3 @@
-import java.lang.ProcessBuilder.Redirect.Type;
 import java.util.*;
 
 
@@ -6,7 +5,7 @@ public class Solution {
     public int[] solution(int []arr) {
         ArrayList<Integer> tmp = new ArrayList<>();
         tmp.add(arr[0]);
-        int tmpCnt = 0;
+        int tmpCnt = 0;     // arraylist의 원소와 비교할 인덱스
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] != tmp.get(tmpCnt)){
                 tmp.add(arr[i]);
