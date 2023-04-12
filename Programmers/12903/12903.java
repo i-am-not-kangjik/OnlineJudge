fe8e4784ee0f01801fa55c31e61a26fc641f01f2
Kangjik Kim <rkqqkb@gmail.com>
2023-04-12 09:41:59 +0900

    프로그래머스 12903번 자바

--- Programmers/12903/12903.java
+++ Programmers/12903/12903.java
@@ -1,13 +1,21 @@
+// 내 풀이
+// class Solution {
+//     public String solution(String s) {
+//         String answer = "";
+//         if (s.length() % 2 == 1){
+//             answer += s.charAt(s.length() / 2);
+//         }
+//         else {
+//             answer += s.charAt(s.length() / 2 - 1);
+//             answer += s.charAt(s.length() / 2);
+//         }
+//         return answer;
+//     }
+// }
+
+// 다른 사람의 풀이를 참고한 풀이
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        if (s.length() % 2 == 1){
-            answer += s.charAt(s.length() / 2);
-        }
-        else {
-            answer += s.charAt(s.length() / 2 - 1);
-            answer += s.charAt(s.length() / 2);
-        }
-        return answer;
+        return s.substring((s.length() - 1) / 2, s.length() / 2 + 1);
     }
 }
