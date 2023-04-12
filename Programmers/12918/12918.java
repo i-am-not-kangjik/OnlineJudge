49cf524b1fb569d649a1b6b5088691b4f6d42fe9
Kangjik Kim <rkqqkb@gmail.com>
2023-04-12 17:50:14 +0900

    프로그래머스 12918번 복기

--- Programmers/12918/12918.java
+++ Programmers/12918/12918.java
@@ -1,16 +1,25 @@
+// 내가 푼 코드
+// class Solution {
+//     public boolean solution(String s) {
+//         boolean answer = true;
+//         if (s.length() != 4 && s.length() != 6){
+//             answer = false;
+//             return answer;
+//         }
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) < 48 || s.charAt(i) > 57){
+//                 answer = false;
+//                 return answer;
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
+// 다른 사람이 푼 코드
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        if (s.length() != 4 && s.length() != 6){
-            answer = false;
-            return answer;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) < 48 || s.charAt(i) > 57){
-                answer = false;
-                return answer;
-            }
-        }
-        return answer;
+      return (s.length() != 4 && s.length() != 6) || (s.split("[0-9]").length > 0) ? false:true;
     }
-}
+  }
+  
