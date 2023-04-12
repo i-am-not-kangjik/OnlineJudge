7e58cee8099530ce4f4dd165ebd1bf77634439d2
Kangjik Kim <rkqqkb@gmail.com>
2023-04-12 14:06:46 +0900

    프로그래머스 12937번 복기

--- Programmers/12937/12937.java
+++ Programmers/12937/12937.java
@@ -1,9 +1,18 @@
+// // 내가 한 풀이
+// class Solution {
+//     public String solution(int num) {
+//         String answer = "Odd";
+//         if (num % 2 == 0){
+//             answer = "Even";
+//         }
+//         return answer;
+//     }
+// }
+
+// 다른 사람의 풀이를 참고한 풀이
+
 class Solution {
     public String solution(int num) {
-        String answer = "Odd";
-        if (num % 2 == 0){
-            answer = "Even";
-        }
-        return answer;
+        return (num % 2 == 0) ? "Even" : "Odd";
     }
 }
