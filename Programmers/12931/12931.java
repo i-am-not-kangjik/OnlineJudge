fb1354834ec1cf3e94c2a24f03b20e44eec33140
Kangjik Kim <rkqqkb@gmail.com>
2023-04-12 17:34:37 +0900

    프로그래머스 12931번

--- Programmers/12931/12931.java
+++ Programmers/12931/12931.java
@@ -1,14 +1,33 @@
+// 내가 작성한 코드
+// import java.util.*;
+
+// public class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+
+//        String tmp = Integer.toString(n);
+//        for (int i = 0; i < tmp.length(); i++) {
+//             answer +=  Character.getNumericValue(tmp.charAt(i)) ;
+//        }
+
+//         return answer;
+//     }
+// }
+
+// 다른 사람의 풀이를 참고한 코드
 import java.util.*;
 
 public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-       String tmp = Integer.toString(n);
-       for (int i = 0; i < tmp.length(); i++) {
-            answer +=  Character.getNumericValue(tmp.charAt(i)) ;
-       }
+        while(true){
+            answer+=n%10;
+            if(n<10)
+                break;
 
+            n=n/10;
+        }
         return answer;
     }
 }
