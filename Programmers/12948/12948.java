2b86a8776bf1cfc610431ae20f7990a827448c79
Kangjik Kim <rkqqkb@gmail.com>
2023-04-13 12:07:36 +0900

    프로그래머스 12948번 복기

--- Programmers/12948/12948.java
+++ Programmers/12948/12948.java
@@ -1,10 +1,22 @@
-class Solution {
-    public String solution(String phone_number) {
-        String tmp = phone_number.substring(0, phone_number.length()-4)
-        .replaceAll("[0-9]", "*");
+// 내 풀이
+// class Solution {
+//     public String solution(String phone_number) {
+//         String tmp = phone_number.substring(0, phone_number.length()-4)
+//         .replaceAll("[0-9]", "*");
         
-        String answer = tmp + phone_number.substring(phone_number.length()-4);
+//         String answer = tmp + phone_number.substring(phone_number.length()-4);
 
-        return answer;
+//         return answer;
+//     }
+// }
+
+// 다른 사람의 풀이를 참고한 풀이
+class Solution{
+    public String solution(String phone_number) {
+        char[] ch = phone_number.toCharArray();
+        for (int i = 0; i < ch.length - 4; i++) {
+            ch[i] = '*';
+        }
+        return String.valueOf(ch);
     }
 }
